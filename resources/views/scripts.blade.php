17a74523a0f633e4600ad39cbe73944a71f1f06c
Victory Osayi <lavictorybiz@gmail.com>
2021-08-09 02:27:00 +0100

    WIP

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -1,6 +1,6 @@
 @isset($jsPath)
   <script>
-    const laravelTelInputConfig = @json(config('tel-input.options'));
+    const laravelTelInputConfig = @json(config('laravel-tel-input.options'));
     {!! file_get_contents($jsPath) !!}
   </script>
 @endisset
